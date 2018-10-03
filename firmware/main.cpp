acdab49fd0af2c765e4d099106c86ea32f2f1464
Nathan Robinson <nrobinson2000@me.com>
2018-10-02 22:08:44 -0400

    at 22:08 of 2018-02-10

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,11 +1,67 @@
 #include "Particle.h"
 
-void setup() // Put setup code here to run once
-{
+int relay[5] = {D4,D5,D6,D7,D8};
 
+//our events are called when IFTTT applets are triggered
+void offHandler(const char *event, const char *data)
+{
+        for (int i = 0; i < 5; i++) // Setup GPIO
+        {
+                digitalWrite(relay[i], LOW); // set LOW
+        }
+}
+void onHandler(const char *event, const char *data)
+{
+        for (int i = 0; i < 5; i++) // Setup GPIO
+        {
+                digitalWrite(relay[i], HIGH); // set LOW
+        }
 }
 
-void loop() // Put code here to loop forever
+int setRelay(String args)
 {
+        bool relayState;
+        int commaLoc = args.indexOf(",");
+
+        String relayString = args.substring(0,commaLoc);
+        String stateString = args.substring(commaLoc+1);
+
+        Particle.publish("log", relayString + " " + stateString);
+
+        if (stateString.equals("on")) {
+                relayState = LOW;
+        }
+        else {
+                relayState = HIGH;
+        }
+
+        int relayNumber = relayString.toInt();
+
+        pinMode(relay[relayNumber-1], OUTPUT); // set as output
+        digitalWrite(relay[relayNumber-1], relayState);
+
+        return 0;
+}
+
+// setup() is run only once, it's where we set up GPIO and initialise peripherals
+void setup() {
+
+        for (int i = 0; i < 5; i++) // Setup GPIO
+        {
+                pinMode(relay[i], OUTPUT); // set as output
+                digitalWrite(relay[i], HIGH); // set HIGH
+        }
+
+        // Subscribe to events published by IFTTT using Particle.subscribe
+        Particle.subscribe("LOWERPATH-OFFxxx", offHandler); // turning off .. Want to vary this as Relay 01, 02 etc
+        Particle.subscribe("LOWERPATH-ONxxx", onHandler); // turning on...I Want to add more commands to switch differing relays.
+
+        Particle.function("setRelay", setRelay);
+}
+
+// loop() runs continuously, it's our infinite loop.
+void loop() {
+
+// empty because handlers and cloud function manage the relays
 
 }
